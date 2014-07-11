634f455c4670ed8b690c84bb7ca4d27eed63b789
Gitthere <ichi4g@gmail.com>
2014-07-10 20:01:30 -1000

    each input is now different color

--- js/app.js
+++ js/app.js
@@ -4,19 +4,19 @@ $(document).ready(function() {
     //If this method is called, the default action of the event will not be triggered.
     e.preventDefault();
     //gets value entered in username
-    var name = $('#username').val();
-        
+    var name = $('#username').val();   
     //gets value entered in message box
     var message = $('#messageArea').val();
     //creates timestamp
     var time = new Date($.now());
     //appends message containing username, message and timestamp
-    $('#messageWindow').append('<li>' + name +':  ' + message + ':  ' + time + '</li>');
-    $('#messageWindow').append('<span class="user">' + name + '</span>');
-    //$('#messageWindow > span').css('color', 'orange');
-        //console.log($('#messageWindow, span').css('color', 'orange'));
-
-
+    var saveName = $('<li></li>').append('<span class="user">' + name +  ":  " +'</span>');
+    //appends input message to li
+    saveName.append('<span class="messageSend">' + message + ":  " + '</span>');
+    //appends time to li
+    saveName.append('<span class="messageTime">' + time + ":  " + '</span>');
+    //appends completed li to message window
+    $('#messageWindow').append(saveName);
     //clears username and message values onclick
     $('#username').val('');
     $('#messageArea').val('');
