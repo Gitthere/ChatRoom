bb38360926c572a9e453f09aa346b5049eb0bef4
Gitthere <ichi4g@gmail.com>
2014-07-10 19:24:34 -1000

    working on getting name, msg and time to be different colors

--- js/app.js
+++ js/app.js
@@ -1,15 +1,22 @@
 $(document).ready(function() {
 
   $('#submitButton').on('click', function(e) {
+    //If this method is called, the default action of the event will not be triggered.
     e.preventDefault();
     //gets value entered in username
-    var first = $('#username').val();
+    var name = $('#username').val();
+        
     //gets value entered in message box
     var message = $('#messageArea').val();
     //creates timestamp
     var time = new Date($.now());
     //appends message containing username, message and timestamp
-    $('#messageWindow').append('<p>' + first +':  ' + message + ':  ' + time + '</p>');
+    $('#messageWindow').append('<li>' + name +':  ' + message + ':  ' + time + '</li>');
+    $('#messageWindow').append('<span class="user">' + name + '</span>');
+    //$('#messageWindow > span').css('color', 'orange');
+        //console.log($('#messageWindow, span').css('color', 'orange'));
+
+
     //clears username and message values onclick
     $('#username').val('');
     $('#messageArea').val('');
